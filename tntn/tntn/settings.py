a26ef71c28b0a2b18f05d4b0496dc1b00becfc55
hung222 <90371118+hung222@users.noreply.github.com>
2022-12-03 21:53:01 +0700

    Update settings.py

--- tntn/tntn/settings.py
+++ tntn/tntn/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8#r3r!rh#k820qt7^o*-%)0g^!^d44+$!ewdtmesgj935xdu-5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -115,7 +115,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
