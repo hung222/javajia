d1bf975e4d16f286363cd8bef958ceda3ca5a205
hung222 <90371118+hung222@users.noreply.github.com>
2022-12-06 15:39:11 +0700

    Update settings.py

--- tntn/tntn/settings.py
+++ tntn/tntn/settings.py
@@ -13,6 +13,13 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import os
+from dotenv import load_dotenv
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(os.path.join(BASE_DIR, ".env"))
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
